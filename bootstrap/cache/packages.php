c577cd1287581ae44774807385e3c4ab700feb1f
sarthaksinha02580 <44203600+sarthaksinha0280@users.noreply.github.com>
2020-05-01 02:35:20 +0530

    Add files via upload

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,48 @@
+<?php return array (
+  'beyondcode/laravel-dump-server' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BeyondCode\\DumpServer\\DumpServerServiceProvider',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'thomaswelton/laravel-gravatar' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Thomaswelton\\LaravelGravatar\\LaravelGravatarServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Gravatar' => 'Thomaswelton\\LaravelGravatar\\Facades\\Gravatar',
+    ),
+  ),
+);
